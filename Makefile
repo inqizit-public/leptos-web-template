a0690ec5f04c5bdd1d5992e93c110a6339e8872a
Gaurav Sharma <gaurav.sharma@devrev.ai>
2023-09-24 13:55:48 +0530

    fix

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: build serve 
+
+build: 
+	trunk build
+
+serve: 
+	trunk serve --open
+
+deploy: build 
+	-rm -rf tmp
+	git clone https://github.com/inqizit-public/leptos-web-template.git tmp
+	# -rm tmp/*
+	# -cp dist/* tmp
+	cd tmp; git checkout gh-pages; rm *; cp ../dist/* .; git add .; git commit -m "deploy"; git push
+	-rm -rf tmp
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
 use leptos::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    // a "derived signal" is a function that accesses other signals
+    // we can use this to create reactive values that depend on the
+    // values of one or more other signals
+    let double_count = move || count.get() * 2;
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count.get() % 2 == 1
+        >
+            "Click me"
+        </button>
+        // NOTE: self-closing tags like <br> need an explicit /
+        <br/>
+
+        // We'll update this progress bar every time `count` changes
+        <progress
+            // static attributes work as in HTML
+            max="50"
+
+            // passing a function to an attribute
+            // reactively sets that attribute
+            // signals are functions, so this <=> `move || count.get()`
+            value=count
+        >
+        </progress>
+        <br/>
+
+        // This progress bar will use `double_count`
+        // so it should move twice as fast!
+        <progress
+            max="50"
+            // derived signals are functions, so they can also
+            // reactive update the DOM
+            value=double_count
+        >
+        </progress>
+        <p>"Count: " {count}</p>
+        <p>"Double Count: " {double_count}</p>
+    }
+}
+
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    leptos::mount_to_body(App)
 }
